aa29d25e19e671b0b9ef72489a278c6e3b2e789c
yassine4real <yssnabbou2002@gmail.com>
2024-12-01 00:57:56 +0100

    question7

--- src/main/java/net/yassine/tp3/ServletTraitement.java
+++ src/main/java/net/yassine/tp3/ServletTraitement.java
@@ -16,6 +16,7 @@ public class ServletTraitement extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String nom = request.getParameter("nom");
         String prenom = request.getParameter("prenom");
+        String sex = request.getParameter("sex");
 
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
@@ -34,7 +35,7 @@ public class ServletTraitement extends HttpServlet {
                 "        </style>");
 
 
-        out.println("<h1>" + "Bonjour " + nom + " " + prenom + "</h1>");
+        out.println("<h1>" + "Bonjour "+ sex + " " + nom + " " + prenom + "</h1>");
         out.println("</body></html>");
 
         out.close();
