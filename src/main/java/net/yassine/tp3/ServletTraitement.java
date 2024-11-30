f6a0e02202223680095bc08f5c49d27e1339dfbc
yassine4real <yssnabbou2002@gmail.com>
2024-11-30 23:35:17 +0100

    add ServletTraitement and implement code inside it

---
+++ src/main/java/net/yassine/tp3/ServletTraitement.java
@@ -0,0 +1,41 @@
+package net.yassine.tp3;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("ServletTraitement")
+public class ServletTraitement extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String nom = request.getParameter("nom");
+        String prenom = request.getParameter("prenom");
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>\n" +
+                "        <style>\n" +
+                "                body {\n" +
+                "            background-color: linen;\n" +
+                "        }\n" +
+                "\n" +
+                "        h1 {\n" +
+                "            text-align: center;\n" +
+                "            color: maroon;\n" +
+                "            margin-left: 40px;\n" +
+                "        }\n" +
+                "        </style>");
+
+
+        out.println("<h1>" + "Bonjour" + nom + prenom + "</h1>");
+        out.println("</body></html>");
+
+        out.close();
+    }
+}
